05bc8186fee1d353e989fdbb877ccfe339036501
dhiego22 <42843427+dhiego22@users.noreply.github.com>
2022-03-26 20:23:40 -0300

    Add files via upload

--- dependencies.sh
+++ dependencies.sh
@@ -1,5 +1,12 @@
 pip install Streamlit
 pip install pandas
-pip install Lenvenshtein
+pip install python-Lenvenshtein
 pip install streamlit-agraph
 pip install networkx
+pip install xgboost
+pip install -U scikit-learn
+pip install plotly
+pip install matplotlib
+pip install seaborn
+pip install umap-learn
+pip install mrmr_selection
