e1532f1a3888a63c0b70e51e3d9f827ec4954173
dhiego22 <dsouto49@gmail.com>
2022-03-30 12:10:47 -0300

    stcache fs

--- gentle.py
+++ gentle.py
@@ -629,111 +629,122 @@ def feature_selection(X, y):
     
     start_time = datetime.now()
     st.markdown(f'<h1 style="color:red;font-size:30px;">{"Feature selection methods"}</h1>', unsafe_allow_html=True)
-    num_features = 3# Number of features
 
-    my_bar = st.progress(0) 
-    # Pearson
-    def corrrelation_selector(X, y, num_feats):
-            cor_list = []
-            feature_name = X.columns.tolist()
-            # calculate the correlation with y for each feature
-            for i in X.columns.tolist():
-                cor = np.corrcoef(X[i], y)[0, 1]
-                cor_list.append(cor)
-            # replace NaN with 0
-            cor_list = [0 if np.isnan(i) else i for i in cor_list]
-            # feature name
-            cor_feature = X.iloc[:,np.argsort(np.abs(cor_list))[-num_feats:]].columns.tolist()
-            # feature selection? 0 for not select, 1 for select
-            cor_support = [True if i in cor_feature else False for i in feature_name]
-            return cor_support, cor_feature
+    st.cache(suppress_st_warning=True)
+    def get_feateres(X, y):
+        num_features = 3# Number of features
+        
+        my_bar = st.progress(0) 
+        
+        # Pearson
+        def corrrelation_selector(X, y, num_feats):
+                cor_list = []
+                feature_name = X.columns.tolist()
+                # calculate the correlation with y for each feature
+                for i in X.columns.tolist():
+                    cor = np.corrcoef(X[i], y)[0, 1]
+                    cor_list.append(cor)
+                # replace NaN with 0
+                cor_list = [0 if np.isnan(i) else i for i in cor_list]
+                # feature name
+                cor_feature = X.iloc[:,np.argsort(np.abs(cor_list))[-num_feats:]].columns.tolist()
+                # feature selection? 0 for not select, 1 for select
+                cor_support = [True if i in cor_feature else False for i in feature_name]
+                return cor_support, cor_feature
 
 
-    cor_support, embeded_feature = corrrelation_selector(X, y, num_features)
-    scores = list(range(len(embeded_feature),0,-1))
-    rank_dataframe1 = pd.DataFrame()
-    rank_dataframe1['features'] = embeded_feature
-    rank_dataframe1['Pearson scores'] = scores
-    
-    my_bar.progress(25)
-    
-    # Ridge
-    embeded_selector = SelectFromModel(LogisticRegression(C=1, penalty='l2'), max_features=num_features)
-    embeded_selector.fit(X, y)
-    embeded_support = embeded_selector.get_support()
-    embeded_feature = X.loc[:,embeded_support].columns.tolist()
-    scores = list(range(len(embeded_feature),0,-1))
-    rank_dataframe2 = pd.DataFrame()
-    rank_dataframe2['features'] = embeded_feature
-    rank_dataframe2['Ridge scores'] = scores
-    final_rank_df = pd.merge(rank_dataframe1, rank_dataframe2, how = 'outer', on='features')
+        cor_support, embeded_feature = corrrelation_selector(X, y, num_features)
+        scores = list(range(len(embeded_feature)-1,-1,-1))
+        rank_dataframe1 = pd.DataFrame()
+        rank_dataframe1['features'] = embeded_feature
+        rank_dataframe1['Pearson scores'] = scores
+        
+        my_bar.progress(25)
+        
+        # Ridge
+        embeded_selector = SelectFromModel(LogisticRegression(C=1, penalty='l2'), max_features=num_features)
+        embeded_selector.fit(X, y)
+        embeded_support = embeded_selector.get_support()
+        embeded_feature = X.loc[:,embeded_support].columns.tolist()
+        scores = list(range(len(embeded_feature)-1,-1,-1))
+        rank_dataframe2 = pd.DataFrame()
+        rank_dataframe2['features'] = embeded_feature
+        rank_dataframe2['Ridge scores'] = scores
+        final_rank_df = pd.merge(rank_dataframe1, rank_dataframe2, how = 'outer', on='features')
 
-    my_bar.progress(50)
+        my_bar.progress(50)
 
-    # XGBoost
-    embeded_selector = SelectFromModel(xgb.XGBClassifier(eval_metric='mlogloss', random_state = 42), max_features=num_features)
-    embeded_selector.fit(X, y)
-    embeded_support = embeded_selector.get_support()
-    embeded_feature = X.loc[:,embeded_support].columns.tolist()
-    scores = list(range(len(embeded_feature),0,-1))
-    rank_dataframe3 = pd.DataFrame()
-    rank_dataframe3['features'] = embeded_feature
-    rank_dataframe3['XGBoost scores'] = scores
-    final_rank_df = pd.merge(final_rank_df, rank_dataframe3, how = 'outer', on='features')
-   
-    my_bar.progress(75)
+        # XGBoost
+        embeded_selector = SelectFromModel(xgb.XGBClassifier(eval_metric='mlogloss', random_state = 42), max_features=num_features)
+        embeded_selector.fit(X, y)
+        embeded_support = embeded_selector.get_support()
+        embeded_feature = X.loc[:,embeded_support].columns.tolist()
+        scores = list(range(len(embeded_feature)-1,-1,-1))
+        rank_dataframe3 = pd.DataFrame()
+        rank_dataframe3['features'] = embeded_feature
+        rank_dataframe3['XGBoost scores'] = scores
+        final_rank_df = pd.merge(final_rank_df, rank_dataframe3, how = 'outer', on='features')
+       
+        my_bar.progress(75)
 
-    # Boruta
-    model = RandomForestClassifier(n_estimators=100, max_depth=4, random_state=42)
-    model.fit(X, y)
-    feat_selector = BorutaPy(
-        #alpha=0.05,
-        verbose=0, # verbose : int, default=0, 0: no output, 1: displays iteration number, 2: which features have been selected already
-        estimator=model,
-        n_estimators='auto', #num_features
-        max_iter=100  # number of iterations to perform
-    )
-    feat_selector.fit(np.array(X), np.array(y))
-    selected_features = feat_selector.support_
-    embeded_feature = X.loc[:,feat_selector.support_].columns.tolist()
-    scores = list(range(len(embeded_feature), 0,-1)) # Ranking decreasing of Selected Features
-    rank_dataframe4 = pd.DataFrame()
-    rank_dataframe4['features'] = embeded_feature
-    rank_dataframe4['Boruta scores'] = scores
-    final_rank_df = pd.merge(final_rank_df, rank_dataframe4, how = 'outer', on='features')
+        # Boruta
+        # model = RandomForestClassifier(n_estimators=100, max_depth=4, random_state=42)
+        # model.fit(X, y)
+        # feat_selector = BorutaPy(
+        #     #alpha=0.05,
+        #     verbose=0, # verbose : int, default=0, 0: no output, 1: displays iteration number, 2: which features have been selected already
+        #     estimator=model,
+        #     n_estimators='auto', #num_features
+        #     max_iter=100  # number of iterations to perform
+        # )
+        # feat_selector.fit(np.array(X), np.array(y))
+        # selected_features = feat_selector.support_
+        # embeded_feature = X.loc[:,feat_selector.support_].columns.tolist()
+        # num_other_feature = num_features - len(embeded_feature)
+        # scores = list(range(num_features, num_other_feature,-1)) # Ranking decreasing of Selected Features
+        # rank_dataframe4 = pd.DataFrame()
+        # rank_dataframe4['features'] = embeded_feature
+        # rank_dataframe4['Boruta scores'] = scores
+        # final_rank_df = pd.merge(final_rank_df, rank_dataframe4, how = 'outer', on='features')
+        # 
 
-    # mRMR
-    y = pd.Series(y)
-    y.index = X.index
-    selected_features = mrmr.mrmr_classif(X = X, y = y, K = num_features)
-    embeded_feature = X.loc[:,selected_features].columns.tolist()
-    scores = list(range(len(embeded_feature),0,-1))
-    rank_dataframe5 = pd.DataFrame()
-    rank_dataframe5['features'] = embeded_feature
-    rank_dataframe5['mRMR scores'] = scores
-    final_rank_df = pd.merge(final_rank_df, rank_dataframe5, how = 'outer', on = 'features')
-    final_rank_df = final_rank_df.fillna(0)
-    final_rank_df = final_rank_df.groupby(['features']).sum()
-    final_rank_df['features'] = final_rank_df.index
-    my_bar.progress(100)
+        # mRMR
+        y = pd.Series(y)
+        y.index = X.index
+        selected_features = mrmr.mrmr_classif(X = X, y = y, K = num_features)
+        embeded_feature = X.loc[:,selected_features].columns.tolist()
+        scores = list(range(len(embeded_feature)-1,-1,-1))
+        rank_dataframe5 = pd.DataFrame()
+        rank_dataframe5['features'] = embeded_feature
+        rank_dataframe5['mRMR scores'] = scores
+        final_rank_df = pd.merge(final_rank_df, rank_dataframe5, how = 'outer', on = 'features')
+        final_rank_df = final_rank_df.fillna(0)
+        final_rank_df = final_rank_df.groupby(['features']).sum()
+        final_rank_df['features'] = final_rank_df.index
+        my_bar.progress(100)
+        
+        return final_rank_df
+
+    frdf = get_feateres(X, y)
 
     # Merged scores
-    final_rank_df['sum'] = final_rank_df[list(final_rank_df.columns)].sum(axis=1)
-    final_rank_df = final_rank_df.sort_values(by=['sum'], ascending=False)
-    if final_rank_df.empty:
+    st.session_state['final_rank'] = frdf
+    st.session_state['final_rank']['sum'] = st.session_state['final_rank'][list(st.session_state['final_rank'].columns)].sum(axis=1)
+    st.session_state['final_rank'] = st.session_state['final_rank'].sort_values(by=['sum'], ascending=False)
+    if st.session_state['final_rank'].empty:
         st.markdown(f'<h1 style="color:black;font-size:20px;">{"The methods could not find any feature with predictive power"}</h1>', unsafe_allow_html=True)
     else:
-        final_rank_df.index = range(1,len(final_rank_df.index)+1)
-        first_column = final_rank_df.pop('features')
-        final_rank_df = final_rank_df.astype(int)
-        final_rank_df.insert(0, 'features', first_column)
-        st.dataframe(final_rank_df)
-        st.download_button("Press the button to download dataframe with the scores of the features", final_rank_df.to_csv().encode('utf-8'), "file.csv", "text/csv", key='download-csv')
+        st.session_state['final_rank'].index = range(1,len(st.session_state['final_rank'].index)+1)
+        first_column = st.session_state['final_rank'].pop('features')
+        st.session_state['final_rank'] = st.session_state['final_rank'].astype(int)
+        st.session_state['final_rank'].insert(0, 'features', first_column)
+        st.dataframe(st.session_state['final_rank'])
+        st.download_button("Press the button to download dataframe with the scores of the features", st.session_state['final_rank'].to_csv().encode('utf-8'), "file.csv", "text/csv", key='download-csv')
         time_elapsed = datetime.now() - start_time 
         st.write('Time elapsed (hh:mm:ss.ms) {}'.format(time_elapsed) + "\n")
 
-        st.sidebar.markdown(f'<h1 style="color:blue;font-size:18px;">{"Select the features that you want to validate with some classifiers. <br/> Choosing 3 features you can them see in a 3D scatter plot. <br/> (Top 3 Features Selected as default.)</h1>"}', unsafe_allow_html=True)
-        options = st.sidebar.multiselect('', list(st.session_state['main'].drop(['sample', 'label'], axis=1).columns), default=list(final_rank_df.features[:3]))
+        st.sidebar.markdown(f'<h1 style="color:blue;font-size:18px;">{"Select the features that you want to validate with some classifiers. <br/> Choosing 3 features you can them see in a 3D scattern plot. <br/> (Top 3 Features Selected as default.)</h1>"}', unsafe_allow_html=True)
+        options = st.sidebar.multiselect('', list(st.session_state['main'].drop(['sample', 'label'], axis=1).columns), default=list(st.session_state['final_rank'].features[:3]))
 
         if len(options) == 3:
             X_ = st.session_state['main'].drop(['sample', 'label'], axis=1)
@@ -742,6 +753,7 @@ def feature_selection(X, y):
             X_.index = st.session_state['input_dataframe'].index
             
             format_mapping = {"inverse_simpson": "{:.2E}", "hillnumbers": "{:.2E}"}
+            st.markdown(f'<h1 style="color:black;font-size:20px;">{"Dataframe with the top 3 features"}</h1>', unsafe_allow_html=True)
             st.dataframe(X_.style.format(format_mapping))
             fig = px.scatter_3d(X_, x=options[0], y=options[1], z=options[2], color='label')
             st.write(fig)
